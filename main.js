9960ace542edf84a65371c290a084dc72f9d120b
GreyWoulf <93624560+GreyWoulf@users.noreply.github.com>
2022-05-02 16:44:45 -0400

    added if statements, name variable, randnumber variable, and userQuestion variable

---
+++ main.js
@@ -0,0 +1,9 @@
+let userName = "";
+let userQuestion = "When will I be rich?";
+let randomNumber = Math.floor(Math.random() * 8);
+if(userName === 'Jane'){
+    console.log(`Hello, ${userName}. Your Question Was: ${userQuestion}`)
+}
+else {
+    console.log(`Hello. Your Question Was: ${userQuestion}`)
+}
