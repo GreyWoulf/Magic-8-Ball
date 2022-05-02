dacfe2583e2a79a428207df1d28cb4198ab39288
GreyWoulf <93624560+GreyWoulf@users.noreply.github.com>
2022-05-02 17:07:27 -0400

    updated to proper formatting criteria

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let userName = "";
 let userQuestion = "When will I be rich?";
 let randomNumber = Math.floor(Math.random() * 8);
-let eightBall = 0;
+let eightBall = "";
 if(userName === 'Jane'){
     console.log(`Hello, ${userName}. Your Question Was: ${userQuestion}`)
 }
@@ -10,33 +10,28 @@ else {
 }
 
 if (randomNumber === 1){
-    eightBall += randomNumber;
-    console.log(`It is certain`)
+    eightBall += "It is certain";
 }
 else if (randomNumber === 2){
-    eightBall += randomNumber;
-    console.log(`It is decidedly so`)
+    eightBall += `It is decidedly so`
 }
 else if (randomNumber === 3){
-    eightBall += randomNumber;
-    console.log(`Reply hazy try again`)
+    eightBall += "Reply hazy try again";
 }
 else if (randomNumber === 4){
-    eightBall += randomNumber;
-    console.log(`Cannot predict now`)
+    eightBall += "Cannot predict now";
 }
 else if (randomNumber === 5){
-    eightBall += randomNumber;
-    console.log(`Do not count on it`)
+    eightBall += "Do not count on it";
 }
 else if (randomNumber === 6){
-    eightBall += randomNumber;
-    console.log(`My sources say no`)
+    eightBall += "My sources say no";
 }
 else if (randomNumber === 7){
-    eightBall += randomNumber;
-    console.log(`Outlook not so good`)
+    eightBall += "Outlook not so good";
 }
 else {
-    console.log('Signs point to yes')
+    eightBall += "Signs point to yes";
 }
+
+console.log(eightBall);
